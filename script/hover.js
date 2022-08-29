eb208f87789191e8f69583e716f8bfa5f139027d
unknown <cusi.07@live.com.ar>
2022-08-29 00:38:14 -0300

    portfolio

---
+++ script/hover.js
@@ -0,0 +1,11 @@
+
+// const js = document.querySelector('.habilidades.js');
+// js.addEventListener('mouseenter', () => {
+//     js.src = 'https://res.cloudinary.com/cusi/image/upload/v1661658681/js_t5mf5z.png';
+//     document.querySelector('.name.Js').visibility = 'visible';
+
+//     js.addEventListener('mouseleave', () => {
+//         js.src = 'https://res.cloudinary.com/cusi/image/upload/v1661658689/js_b_c5phtj.png';
+//         document.querySelector('.name.Js').visibility = 'visible';
+//     })
+// });
